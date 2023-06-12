e455e6327d43de3a5306df20ed3089cacd463599
jovemcarlosti <carlosandre116@outlook.com>
2023-06-11 22:01:02 -0300

    Interagindo com o usuário (useState)

--- src/components/CampoTexto/index.js
+++ src/components/CampoTexto/index.js
@@ -1,10 +1,18 @@
 import './CampoTexto.css';
 
 const CampoTexto = (props) => {
+    
+    const aoDigitar = (e) => {
+        props.aoAlterado(e.target.value)
+    }
+
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input placeholder={props.placeholder} required={props.obrigatorio}/>
+            <input onChange={aoDigitar}
+            placeholder={`${props.placeholder}...`}
+            required={props.obrigatorio}
+            value={props.valor} />
         </div>
     )
 }
