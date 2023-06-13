2884289dcb2a13664e0bc10fbbbe619856e8a0d6
jovemcarlosti <carlosandre116@outlook.com>
2023-06-13 18:57:04 -0300

    Criando componente Colaborador

--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -1,7 +1,9 @@
+import Colaborador from '../Colaborador';
 import './Time.css';
 
 const Time = (props) => {
     return (
+        props.colaboradores.length > 0 &&
         <section className='time'
             style={{ backgroundColor: props.corSecundaria }}
         >
@@ -10,6 +12,15 @@ const Time = (props) => {
             >
                 {props.nome}
             </h3>
+            <div className='colaboradores'>
+                {props.colaboradores.map(colaborador =>
+                    <Colaborador 
+                        nome={colaborador.nome}
+                        cargo={colaborador.cargo}
+                        imagem={colaborador.imagem}
+                    />
+                )}
+            </div>
         </section>
     )
 }
